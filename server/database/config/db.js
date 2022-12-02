ef78336e3811b310fcaefda8512f5b24fa1f3cf7
adhithyan.mg <adhithyan.mg@zohocorp.com>
2022-12-02 23:28:25 +0530

    setting up the database

---
+++ server/database/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const mongodb_username = encodeURIComponent(process.env.MONGODB_USER);
+const mongodb_password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+const db = `mongodb+srv://${mongodb_username}:${mongodb_password}@cluster0.5udgl4z.mongodb.net/?retryWrites=true&w=majority`;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true 
+        });
+    } catch (err) {
+        console.error(err.message);
+    }
+};
+  
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
+// get the environmental variables
+require('dotenv').config();
+
+// import the required packages
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = 4000;
 
+// connect to Database.
+const mongoose = require('mongoose');
+const connectDB = require('./database/config/db');
+connectDB();
+
+// express app configurations.
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
-});
+
+/* routes starts here */
+app.get("/", (req,res) => res.send("Home page url"));
+/* routes ends here */
+
+// listen to SERVER_PORT once mongoose made connection to mongoDB
+const PORT = process.env.SERVER_PORT || 4000;
+mongoose.connection.once('open', () => {
+    console.log("Connected to MongoDB");    
+    app.listen(PORT, function() {
+        console.log("Server is running on Port: " + PORT);
+    });
+})
