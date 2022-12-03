c6ecafc4b95ef7e3b91cee6bb8720aa9b5161ad1
adhithyan.mg <adhithyan.mg@zohocorp.com>
2022-12-04 00:06:47 +0530

    process mongodb uri from .env file

--- server/database/config/db.js
+++ server/database/config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
-const mongodb_username = encodeURIComponent(process.env.MONGODB_USER);
-const mongodb_password = encodeURIComponent(process.env.MONGODB_PASSWORD);
-const mongodb_dbname = encodeURIComponent(process.env.MONGODB_DBNAME);
-const db = `mongodb+srv://${mongodb_username}:${mongodb_password}@cluster0.5udgl4z.mongodb.net/${mongodb_dbname}?retryWrites=true&w=majority`;
+// const mongodb_username = encodeURIComponent(process.env.MONGODB_USER);
+// const mongodb_password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+// const mongodb_dbname = encodeURIComponent(process.env.MONGODB_DBNAME);
+// const db = `mongodb+srv://${mongodb_username}:${mongodb_password}@cluster0.5udgl4z.mongodb.net/${mongodb_dbname}?retryWrites=true&w=majority`;
+
+const db = process.env.MONGODB_URI;
 
 const connectDB = async () => {
     try {
